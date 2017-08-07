0dcb6ad2eac17e12977670ab2090b2db39949539
d <dingsong87@gmail.com>
2017-08-07 12:06:35 +0800

    add set status function, now can set status and reason

--- webd.py
+++ webd.py
@@ -64,6 +64,11 @@ class Response(object):
             for key, value in headers.items():
                 self.headers.append((str(key).lower(), value))
 
+    def set_status(self, status_code, reason=None):
+        self.status = ' '.join((str(status_code), HTTP_CODES.get(status_code, 'Unknown')))
+        self.body = reason
+        return self.body
+
 
 class Application(object):
     def __init__(self, request=Request, response=Response):
@@ -166,6 +171,59 @@ class Application(object):
             return [body], len(body)
 
         return [], 0
+
+
+class HTTPStatus(Exception):
+
+    def __init__(self, status, body=None, headers=None):
+        self.status = status
+        self.body = body
+        self.headers = headers
+
+
+HTTP_CODES = {
+    100: 'CONTINUE',
+    101: 'SWITCHING PROTOCOLS',
+    200: 'OK',
+    201: 'CREATED',
+    202: 'ACCEPTED',
+    203: 'NON-AUTHORITATIVE INFORMATION',
+    204: 'NO CONTENT',
+    205: 'RESET CONTENT',
+    206: 'PARTIAL CONTENT',
+    300: 'MULTIPLE CHOICES',
+    301: 'MOVED PERMANENTLY',
+    302: 'FOUND',
+    303: 'SEE OTHER',
+    304: 'NOT MODIFIED',
+    305: 'USE PROXY',
+    306: 'RESERVED',
+    307: 'TEMPORARY REDIRECT',
+    400: 'BAD REQUEST',
+    401: 'UNAUTHORIZED',
+    402: 'PAYMENT REQUIRED',
+    403: 'FORBIDDEN',
+    404: 'NOT FOUND',
+    405: 'METHOD NOT ALLOWED',
+    406: 'NOT ACCEPTABLE',
+    407: 'PROXY AUTHENTICATION REQUIRED',
+    408: 'REQUEST TIMEOUT',
+    409: 'CONFLICT',
+    410: 'GONE',
+    411: 'LENGTH REQUIRED',
+    412: 'PRECONDITION FAILED',
+    413: 'REQUEST ENTITY TOO LARGE',
+    414: 'REQUEST-URI TOO LONG',
+    415: 'UNSUPPORTED MEDIA TYPE',
+    416: 'REQUESTED RANGE NOT SATISFIABLE',
+    417: 'EXPECTATION FAILED',
+    500: 'INTERNAL SERVER ERROR',
+    501: 'NOT IMPLEMENTED',
+    502: 'BAD GATEWAY',
+    503: 'SERVICE UNAVAILABLE',
+    504: 'GATEWAY TIMEOUT',
+    505: 'HTTP VERSION NOT SUPPORTED',
+}
 
 
 request = Request()
