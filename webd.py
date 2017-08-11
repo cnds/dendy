70b39ab6fb0bc46a1f2e8783cfd587c8e7f5882e
d <dingsong87@gmail.com>
2017-08-11 11:42:44 +0800

    optimize http codes dict

--- webd.py
+++ webd.py
@@ -3,47 +3,47 @@ import json
 HTTP_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD']
 
 HTTP_CODES = {
-    100: 'CONTINUE',
-    101: 'SWITCHING PROTOCOLS',
-    200: 'OK',
-    201: 'CREATED',
-    202: 'ACCEPTED',
-    203: 'NON-AUTHORITATIVE INFORMATION',
-    204: 'NO CONTENT',
-    205: 'RESET CONTENT',
-    206: 'PARTIAL CONTENT',
-    300: 'MULTIPLE CHOICES',
-    301: 'MOVED PERMANENTLY',
-    302: 'FOUND',
-    303: 'SEE OTHER',
-    304: 'NOT MODIFIED',
-    305: 'USE PROXY',
-    306: 'RESERVED',
-    307: 'TEMPORARY REDIRECT',
-    400: 'BAD REQUEST',
-    401: 'UNAUTHORIZED',
-    402: 'PAYMENT REQUIRED',
-    403: 'FORBIDDEN',
-    404: 'NOT FOUND',
-    405: 'METHOD NOT ALLOWED',
-    406: 'NOT ACCEPTABLE',
-    407: 'PROXY AUTHENTICATION REQUIRED',
-    408: 'REQUEST TIMEOUT',
-    409: 'CONFLICT',
-    410: 'GONE',
-    411: 'LENGTH REQUIRED',
-    412: 'PRECONDITION FAILED',
-    413: 'REQUEST ENTITY TOO LARGE',
-    414: 'REQUEST-URI TOO LONG',
-    415: 'UNSUPPORTED MEDIA TYPE',
-    416: 'REQUESTED RANGE NOT SATISFIABLE',
-    417: 'EXPECTATION FAILED',
-    500: 'INTERNAL SERVER ERROR',
-    501: 'NOT IMPLEMENTED',
-    502: 'BAD GATEWAY',
-    503: 'SERVICE UNAVAILABLE',
-    504: 'GATEWAY TIMEOUT',
-    505: 'HTTP VERSION NOT SUPPORTED',
+    100: '100 Continue',
+    101: '101 Switching Protocols',
+    200: '200 OK',
+    201: '201 Created',
+    202: '202 Accepted',
+    203: '203 Non-Authoritative Information',
+    204: '204 No Content',
+    205: '205 Reset Content',
+    206: '206 Partial Content',
+    300: '300 Multiple Choices',
+    301: '301 Moved Permanently',
+    302: '302 Found',
+    303: '303 See Other',
+    304: '304 Not Modified',
+    305: '305 Use Proxy',
+    306: '306 Reserved',
+    307: '307 Temporary Redirect',
+    400: '400 Bad Request',
+    401: '401 Unauthorized',
+    402: '402 Payment Required',
+    403: '403 Forbidden',
+    404: '404 Not Found',
+    405: '405 Method Not Allowed',
+    406: '406 Not Acceptable',
+    407: '407 Proxy Authentication Required',
+    408: '408 Request Timeout',
+    409: '409 Conflict',
+    410: '410 Gone',
+    411: '411 Length Required',
+    412: '412 Precondition Failed',
+    413: '413 Request Entity Too Large',
+    414: '414 Request-Uri Too Long',
+    415: '415 Unsupported Media Type',
+    416: '416 Requested Range Not Satisfiable',
+    417: '417 Expectation Failed',
+    500: '500 Internal Server Error',
+    501: '501 Not Implemented',
+    502: '502 Bad Gateway',
+    503: '503 Service Unavailable',
+    504: '504 Gateway Timeout',
+    505: '505 Http Version Not Supported',
 }
 
 DEFAULT_CONTENT_TYPE = 'application/json; charset=utf-8'
@@ -137,11 +137,9 @@ class Response(object):
                 self.headers.append((str(key).lower(), value))
 
     def set_status(self, status_code, reason=None):
-        self.status = ' '.join((str(status_code), HTTP_CODES.get(
-            status_code, 'Unknown')))
+        self.status = HTTP_CODES.get(status_code, 'Unknown')
         self.body = reason
         return self.body
-
 
 
 class Application(object):
@@ -162,7 +160,7 @@ class Application(object):
                 allowed_methods = ', '.join(HTTP_METHODS)
                 response.headers.append(('Allow', allowed_methods))
             else:
-                response.status = '405 METHOD_NOT_ALLOWED'
+                response.status = HTTP_CODES[405]
         else:
             output = responder(**params)
             response.body = json.dumps(output)
