59a42e424fae2050da2539981bc8afc8cdeb88ac
DingSong <dingsong87@gmail.com>
2017-08-27 16:54:54 +0800

    new feature, middleware option on API

--- webd.py
+++ webd.py
@@ -150,27 +150,50 @@ class Response(object):
         return self.body
 
 
-class Api(object):
-    def __init__(self):
+class API(object):
+    def __init__(self, middleware=None):
         self.routes = dict()
+        self._middleware = self.prepare_middleware(middleware)
 
     def __call__(self, environ, start_response):
         request.init(environ)
         response.init()
         headers = response.headers
+        after_stack = list()
 
-        responder, kwargs, method, uri_template = self._get_responder(request)
-        if responder is None:
-            if method == 'HEAD':
-                response.body = ''
-            elif method == 'OPTIONS':
-                allowed_methods = ', '.join(HTTP_METHODS)
-                response.headers.append(('Allow', allowed_methods))
-            else:
-                response.status = HTTP_CODES[405]
+        try:
+            for component in self._middleware:
+                process_before, _, process_after = component
+                if process_before is not None:
+                    process_before(request, response)
+
+                if process_after is not None:
+                    after_stack.append(process_after)
+
+            responder, kwargs, method, uri_template = self._get_responder(request)
+        except Exception as ex:
+            HTTPError(500, ex)
         else:
-            output = responder(**kwargs)
-            response.body = json.dumps(output)
+            for component in self._middleware:
+                _, process_on, _ = component
+                if process_on is not None:
+                    process_on(request, response)
+
+            if responder is None:
+                if method == 'HEAD':
+                    response.body = ''
+                elif method == 'OPTIONS':
+                    allowed_methods = ', '.join(HTTP_METHODS)
+                    response.headers.append(('Allow', allowed_methods))
+                else:
+                    response.status = HTTP_CODES[405]
+            else:
+                output = responder(**kwargs)
+                response.body = json.dumps(output)
+
+        finally:
+            for process_after in after_stack:
+                process_after(request, response)
 
         body, length = self._get_body(response)
         if length is not None:
@@ -256,6 +279,29 @@ class Api(object):
             return [body], len(body)
 
         return list(), 0
+
+    def prepare_middleware(self, middleware=None):
+        prepared_middleware = list()
+
+        if middleware is None:
+            middleware = list()
+        else:
+            if not isinstance(middleware, list):
+                middleware = [middleware]
+
+        for component in middleware:
+            process_before = getattr(component, 'process_before', None)
+            process_on = getattr(component, 'process_on', None)
+            process_after = getattr(component, 'process_after', None)
+
+            if not (process_before or process_on or process_after):
+                raise HTTPError(500,
+                                TypeError('%s is not implemented' % component))
+
+            prepared_middleware.append((process_before, process_on,
+                                        process_after))
+
+        return prepared_middleware
 
 
 class HTTPError(Exception):
