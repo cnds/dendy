db559cd366f63d3e83448590e70fb1882459b183
d <dingsong87@gmail.com>
2017-11-07 11:58:14 +0800

    add jwt base util

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 
-setup(name='dendy',
+setup(name='Dendy',
       version='0.1.0',
-      description='Python web framework',
+      description='Python micro web framework',
       author='cnds',
       author_email='dingsong87@gmail.com',
       license='MIT',
-      url='https://github.com/cnds/dendy'
+      url='https://github.com/cnds/dendy',
+      install_requires=['pyjwt>=1.5.0']
       )
