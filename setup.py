a73de09d747dbd5fb7028fc39c1d1e1160f10af1
d <dingsong87@gmail.com>
2017-11-07 14:18:27 +0800

    optimize setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 
-setup(name='Dendy',
+setup(name='dendy',
       version='0.1.0',
       description='Python micro web framework',
       author='cnds',
       author_email='dingsong87@gmail.com',
       license='MIT',
       url='https://github.com/cnds/dendy',
+      packages=['dendy'],
       install_requires=['pyjwt>=1.5.0']
       )
