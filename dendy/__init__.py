75e351e5a61a0234b9c300b9428926f7b8b02910
d <dingsong87@gmail.com>
2017-11-07 16:56:50 +0800

    optimize setup script

--- dendy/__init__.py
+++ dendy/__init__.py
@@ -1,4 +1,8 @@
 from dendy.api import API
-from dendy.request import Request
-from dendy.response import Response
+from dendy.request import req
+from dendy.response import resp
 from dendy.hooks import before, after
+from dendy.status import HTTPError, HTTP_CODES
+
+
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup
+from dendy import __version__
 
-setup(name='dendy',
-      version='0.1.0',
-      description='Python micro web framework',
-      author='cnds',
-      author_email='dingsong87@gmail.com',
-      license='MIT',
-      url='https://github.com/cnds/dendy',
-      packages=['dendy'],
-      install_requires=['pyjwt>=1.5.0']
-      )
+setup(
+    name='dendy',
+    version=__version__,
+    description='Python micro web framework',
+    author='cnds',
+    author_email='dingsong87@gmail.com',
+    license='MIT',
+    url='https://github.com/cnds/dendy',
+    packages=['dendy'],
+    install_requires=['pyjwt>=1.5.0']
+)
